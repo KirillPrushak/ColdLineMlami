41c143dc7aec2f4f7783ef113f6213b78e4d6400
KirillPrushak <kirillprushak@gmail.com>
2023-04-16 13:08:57 +0300

    Init

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class CharacterController : MonoBehaviour
+    {
+        private Rigidbody _rigidbody;
+        private void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+        }
+
+        public void Move(Vector2 direction)
+        {
+            var force = new Vector3(direction.x, 0, direction.y);
+            _rigidbody.AddForce();
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class PlayerInput : MonoBehaviour
+    {
+        private CharacterController _controller;
+        private void Awake()
+        {
+            _controller = GetComponent<CharacterController>();
+        }
+
+        private void Update()
+        {
+            var forwardInput = 0f;
+            var rightInput = 0f;
+            
+            if (Input.GetKey(KeyCode.W))
+            {
+                forwardInput = 1f;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                forwardInput = -1f;
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                rightInput = -1;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                rightInput = 1f;
+            }
+            
+            _controller.Move(new Vector2(rightInput, forwardInput));
+        }
+    }
+}
