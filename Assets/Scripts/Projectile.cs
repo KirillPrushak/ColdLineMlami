066a432b1da28ad11a638c6657a15872930bffa5
KirillPrushak <kirillprushak@gmail.com>
2023-04-24 20:10:18 +0300

    Add ragdoll

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class Projectile : MonoBehaviour
+    {
+        private void OnCollisionEnter(Collision collision)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
